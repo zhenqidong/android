cbda8ce67f152350705d5bb5dea0b781839e2090
zhenqi dong <zd@notright.com>
2014-09-13 14:50:41 -0400

    splash works

---
+++ src/com/dextroute/android/happymatch/SplashActivity.java
@@ -0,0 +1,55 @@
+package com.dextroute.android.happymatch;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SplashActivity extends Activity {
+
+	//splash screen timer
+	private static int SPLASH_TIME_OUT = 3000;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		
+		new Handler().postDelayed(new Runnable(){
+			/*
+			 * Showing splash with a timer
+			 */
+			@Override
+			public void run(){
+				// This method will be executed once the timer is over
+				// start main
+				
+				Intent i = new Intent(SplashActivity.this,MainActivity.class);
+				startActivity(i);
+				
+				//close this activity
+				finish();
+			}
+		},SPLASH_TIME_OUT);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.splash, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
